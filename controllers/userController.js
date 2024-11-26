8b10774bb638f23ea85c3127007dd08914299b87
motorolla29 <eutyou@gmail.com>
2024-11-26 18:13:05 +0300

    added name field for registation with token

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,29 +4,41 @@ const { User, Basket, Lovelist } = require('../models/models');
 
 const ApiError = require('../error/ApiError');
 
-const generateJwt = (id, email, role) => {
-  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+const generateJwt = (id, name, email, role) => {
+  return jwt.sign({ id, name, email, role }, process.env.SECRET_KEY, {
     expiresIn: '24h',
   });
 };
 
 class UserController {
   async registration(req, res, next) {
-    const { email, password, role } = req.body;
-    if (!email || !password) {
-      return next(ApiError.badRequest('Некорректный email или password'));
+    const { name, email, password, role } = req.body;
+    if (!name) {
+      return next(ApiError.badRequest({ name: 'Incorrect user name' }));
+    }
+    if (!email) {
+      return next(ApiError.badRequest({ email: 'Incorrect email' }));
+    }
+    if (!password) {
+      return next(ApiError.badRequest({ password: 'Incorrect password' }));
     }
     const candidate = await User.findOne({ where: { email } });
+
     if (candidate) {
       return next(
-        ApiError.badRequest('Пользователь с таким email уже существует')
+        ApiError.badRequest({ email: 'A user with this email already exists' })
       );
     }
     const hashPassword = await bcrypt.hash(password, 5);
-    const user = await User.create({ email, role, password: hashPassword });
+    const user = await User.create({
+      name,
+      email,
+      role,
+      password: hashPassword,
+    });
     const basket = await Basket.create({ userId: user.id });
     const lovelist = await Lovelist.create({ userId: user.id });
-    const token = generateJwt(user.id, user.email, user.role);
+    const token = generateJwt(user.id, user.name, user.email, user.role);
     return res.json({ token });
   }
 
@@ -34,11 +46,13 @@ class UserController {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return next(ApiError.internal('Пользователь не найден'));
+      return next(
+        ApiError.internal({ email: 'User with such email not found' })
+      );
     }
     let comparePassword = bcrypt.compareSync(password, user.password);
     if (!comparePassword) {
-      return next(ApiError.internal('Указан неверный пароль'));
+      return next(ApiError.internal({ password: 'Incorrect password' }));
     }
     const token = generateJwt(user.id, user.email, user.role);
     return res.json({ token });
