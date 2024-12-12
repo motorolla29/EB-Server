ce0d20324e67270549385e3e0336f95b22df309b
motorolla29 <eutyou@gmail.com>
2024-12-12 17:42:43 +0300

    db.js ssl  require fix

--- db.js
+++ db.js
@@ -8,7 +8,11 @@ module.exports = new Sequelize(
   {
     dialect: 'postgres',
     dialectModule: pg,
-    dialectOptions: {},
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     ssl: require,
