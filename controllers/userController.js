02e8a3d7b1b165dbdfa1082b616add9ce0099939
motorolla29 <eutyou@gmail.com>
2024-12-12 18:29:53 +0300

    metadata fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -123,7 +123,6 @@ class UserController {
         .rotate() // Исправление ориентации на основе EXIF
         .resize({ width: 1920, height: 1920, fit: 'inside' }) // Ограничиваем разрешение
         .jpeg({ quality: 80 }) // Сжимаем качество до 80%
-        .withMetadata({ exif: false }) // Удаляем EXIF-данные для защиты конфиденциальности
         .toFile(tempFilePath);
 
       // Загрузка файла в Cloudinary
