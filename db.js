294eac12c6c5682fb4ca0b1154bbff7d0ae1a690
motorolla29 <eutyou@gmail.com>
2024-12-26 11:29:47 +0300

    sql config changed

--- db.js
+++ db.js
@@ -7,16 +7,22 @@ module.exports = new Sequelize(
   process.env.DB_PASSWORD,
   {
     dialect: 'postgres',
+    dialectModule: pg,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     pool: {
-      max: 200,
+      max: 5, // Максимальное количество соединений
+      min: 1, // Минимальное количество соединений
+      idle: 10000, // Время простоя (в миллисекундах) перед закрытием соединения
+      acquire: 30000, // Максимальное время (в миллисекундах) ожидания перед таймаутом
     },
     dialectModule: pg,
     dialectOptions: {
       ssl: {
-        require: true,
+        require: true, // Необходимость SSL-соединения
+        rejectUnauthorized: false, // Для Neon отключаем проверку сертификатов
       },
     },
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    logging: false, // Отключить SQL-логирование для повышения читаемости консоли
   }
 );
